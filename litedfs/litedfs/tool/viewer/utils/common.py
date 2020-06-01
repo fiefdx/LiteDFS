39dfee7a6ea3bb6cc9b11943964381cbe207b2ba
fiefdx <fiefdx@163.com>
2020-06-01 20:23:16 +0800

    test local storage listdir function

--- litedfs/litedfs/tool/viewer/utils/common.py
+++ litedfs/litedfs/tool/viewer/utils/common.py
@@ -98,6 +98,12 @@ class MetaNotDictError(Exception):
         self.message = message
 
 
+def sha1sum(content):
+    sha1 = hashlib.sha1()
+    sha1.update(content.encode("utf-8"))
+    return sha1.hexdigest()
+
+
 def file_sha1sum(file_path):
     sha1 = hashlib.sha1()
     with open(file_path, 'rb') as f:
@@ -158,3 +164,140 @@ def init_storage():
     for d in directories:
         if not os.path.exists(d) or not os.path.isdir(d):
             os.makedirs(d)
+
+
+def get_file_size(size):
+    result = ""
+    try:
+        if size > 1024*1014*1024:
+            result = "%.3f G"%(size/1024.0/1024.0/1024.0)
+        elif size > 1024*1024:
+            result = "%.3f M"%(size/1024.0/1024.0)
+        elif size > 1024:
+            result = "%.3f K"%(size/1024.0)
+        else:
+            result = "%d B"%size
+    except Exception as e:
+        LOG.exception(e)
+        result = "0 B"
+    return result
+
+
+def makekey(c):
+    if isinstance(c, int):
+        return c
+    elif isinstance(c, str):
+        return c.lower()
+
+
+def listsort(dirs, files, sort_by = "name", desc = False):
+    dirs_keys = []
+    dirs_tree = {}
+    dirs_sort = []
+    files_keys = []
+    files_tree = {}
+    files_sort = []
+    for d in dirs:
+        dirs_keys.append(d[sort_by])
+        if d[sort_by] in dirs_tree:
+            dirs_tree[d[sort_by]].append(d)
+        else:
+            dirs_tree[d[sort_by]] = []
+            dirs_tree[d[sort_by]].append(d)
+    dirs_keys = list(set(dirs_keys))
+    dirs_keys.sort(key = makekey, reverse = desc)
+    # LOG.info("Dirs_keys: %s", dirs_keys)
+    n = 1
+    for k in dirs_keys:
+        for d in dirs_tree[k]:
+            d["num"] = n
+            d["size"] = get_file_size(d["size"])
+            dirs_sort.append(d)
+            n += 1
+    for f in files:
+        files_keys.append(f[sort_by])
+        if f[sort_by] in files_tree:
+            files_tree[f[sort_by]].append(f)
+        else:
+            files_tree[f[sort_by]] = []
+            files_tree[f[sort_by]].append(f)
+    files_keys = list(set(files_keys))
+    files_keys.sort(key = makekey, reverse = desc)
+    # LOG.info("Files_keys: %s", files_keys)
+    for k in files_keys:
+        for f in files_tree[k]:
+            f["num"] = n
+            f["size"] = get_file_size(f["size"])
+            files_sort.append(f)
+            n += 1
+    return (dirs_sort, files_sort)
+
+
+def listdir(dir_path = ".", key = "", sort_by = "name", desc = False):
+    dirs = []
+    files = []
+    try:
+        dirs_list = [d for d in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, d))]
+        files_list = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+        dirs_list.sort()
+        files_list.sort()
+        n = 1
+        for d in dirs_list:
+            d_path = os.path.join(dir_path, d)
+            dirs.append({
+                "num":n, 
+                "name":d, 
+                "sha1":sha1sum(d_path), 
+                "decrypt_name":"", 
+                "type":"Directory", 
+                "size":os.path.getsize(d_path),
+                "ctime":time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.path.getctime(d_path))),
+                "mtime":time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.path.getmtime(d_path)))
+            })
+            n += 1
+        for f in files_list:
+            decrypt_name = ""
+            if  key != "" and os.path.splitext(f)[-1].lower() == ".crypt":
+                file_path = os.path.join(dir_path, f)
+                crypt = CryptFile(file_path)
+                crypt.open_source_file()
+                decrypt_name = crypt.decrypt_info(key)
+            elif key != "" and os.path.splitext(f)[-1].lower() == ".hide":
+                file_path = os.path.join(dir_path, f)
+                crypt = CryptFile(file_path)
+                crypt.open_source_file()
+                decrypt_name = crypt.show_info(key)
+            f_path = os.path.join(dir_path, f)
+            files.append({
+                "num":n, 
+                "name":f, 
+                "sha1":sha1sum(f_path), 
+                "decrypt_name":decrypt_name, 
+                "type":os.path.splitext(f)[-1], 
+                "size":os.path.getsize(f_path),
+                "ctime":time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.path.getctime(f_path))),
+                "mtime":time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.path.getmtime(f_path)))
+            })
+            n += 1
+    except Exception as e:
+        LOG.exception(e)
+    return listsort(dirs, files, sort_by = sort_by, desc = desc)
+
+
+def joinpath(dir_list):
+    dir_path = dir_list[0]
+    dir_list = dir_list[1:]
+    for d in dir_list:
+        dir_path = os.path.join(dir_path, d)
+    return dir_path
+
+
+def splitpath(dir_path):
+    dir_list = []
+    dir_path, dir_last = os.path.split(dir_path)
+    while dir_last != "":
+        dir_list.append(dir_last)
+        dir_path, dir_last = os.path.split(dir_path)
+    dir_list.append(dir_path)
+    dir_list.reverse()
+    return dir_list
---
+++ litedfs/test/test_local_storage.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+import logging
+
+cwd = os.path.split(os.path.realpath(__file__))[0]
+sys.path.insert(0, os.path.split(cwd)[0])
+
+from litedfs.tool.viewer.utils.common import makekey, listsort, listdir, joinpath, splitpath
+from litedfs.name import logger
+
+LOG = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    logger.config_logging(file_name = "test_local_storage.log",
+                          log_level = "DEBUG",
+                          dir_name = os.path.join(cwd, "logs"),
+                          day_rotate = False,
+                          when = "D",
+                          interval = 1,
+                          max_size = 20,
+                          backup_count = 5,
+                          console = True)
+
+    LOG.debug("test start")
+    
+    try:
+        LOG.debug(listdir("/home/breeze"))
+    except Exception as e:
+        LOG.exception(e)
+
+    LOG.debug("test end")
