869fff773a7d3732d0528dbb3dd8049176610085
fiefdx <fiefdx@163.com>
2020-06-05 19:35:34 +0800

    client support md5 checksum

--- litedfs/litedfs/tool/client.py
+++ litedfs/litedfs/tool/client.py
@@ -5,6 +5,7 @@ import re
 import sys
 import json
 import time
+import hashlib
 import argparse
 import logging
 import random
@@ -13,6 +14,43 @@ from io import BytesIO
 import requests
 
 LOG = logging.getLogger(__name__)
+
+BUF_SIZE = 65536
+
+
+def file_md5sum(file_path):
+    md5 = hashlib.md5()
+    with open(file_path, 'rb') as fp:
+        while True:
+            data = fp.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+    return md5.hexdigest()
+
+
+def bytes_io_md5sum(fp):
+    md5 = hashlib.md5()
+    while True:
+        data = fp.read(BUF_SIZE)
+        if not data:
+            break
+        md5.update(data)
+    return md5.hexdigest()
+
+
+def bytes_md5sum(b):
+    md5 = hashlib.md5()
+    md5.update(b)
+    return md5.hexdigest()
+
+
+def strings_md5sum(l):
+    md5 = hashlib.md5()
+    for s in l:
+        if s:
+            md5.update(s.encode("utf-8"))
+    return md5.hexdigest()
 
 
 class LiteDFSClient(object):
@@ -34,6 +72,7 @@ class LiteDFSClient(object):
                     if "result" in data and data["result"] == "ok":
                         data_nodes = data["data_nodes"]
                         fp = open(local_path, "rb")
+                        blocks_md5 = []
                         for block in data["blocks"]:
                             block_id = block[0]
                             block_size = block[1]
@@ -44,6 +83,10 @@ class LiteDFSClient(object):
                             data_node = data_nodes[str(node_id)]
                             block_create_url = "http://%s:%s/block/create" % (data_node[0], data_node[1])
                             block_content.seek(0)
+                            block_md5 = bytes_io_md5sum(block_content)
+                            block.append(block_md5)
+                            blocks_md5.append(block_md5)
+                            block_content.seek(0)
                             files = {'up_file': ("up_file", block_content, b"text/plain")}
                             values = {"name": data["id"], "block": block_id, "ids": node_ids}
                             r = requests.post(block_create_url, files = files, data = values)
@@ -51,6 +94,9 @@ class LiteDFSClient(object):
                                 d = r.json()
                                 if "result" in d and d["result"] != "ok":
                                     LOG.error("create block failed: %s", d)
+                                    success = False
+                                    break
+                                elif "md5" in d and d["md5"] != block_md5:
                                     success = False
                                     break
                             if not success:
@@ -63,6 +109,7 @@ class LiteDFSClient(object):
                                 "id": data["id"],
                                 "replica": replica,
                                 "blocks": data["blocks"],
+                                "checksum": strings_md5sum(blocks_md5),
                             }
                             r = requests.post("%s/file/create" % self.base_url, json = json_data)
                             if r.status_code == 200:
@@ -171,29 +218,49 @@ class LiteDFSClient(object):
                             data_nodes[int(node_id)] = data["data_nodes"][node_id]
                         file_info = data["file_info"]
                         fp = open(local_path, "wb")
+                        blocks_md5 = []
                         for block in file_info["blocks"]:
                             block_id = block[0]
                             block_size = block[1]
                             node_ids = block[2]
+                            block_md5 = block[3]
                             exists_ids = list(set(node_ids).intersection(set(data_nodes.keys())))
                             if exists_ids:
-                                node_id = random.choice(exists_ids)
-                                data_node = data_nodes[node_id]
-                                block_download_url = "http://%s:%s/block/download?name=%s&block=%s" % (data_node[0], data_node[1], file_info["id"], block_id)
-                                r = requests.get(block_download_url)
-                                if r.status_code == 200:
-                                    fp.write(r.content)
-                                else:
-                                    LOG.error("error:\ncode: %s\ncontent: %s", r.status_code, r.content)
+                                exists_ids_random = random.sample(exists_ids, len(exists_ids))
+                                block_success = True
+                                for node_id in exists_ids_random:
+                                    data_node = data_nodes[node_id]
+                                    block_download_url = "http://%s:%s/block/download?name=%s&block=%s" % (data_node[0], data_node[1], file_info["id"], block_id)
+                                    r = requests.get(block_download_url)
+                                    if r.status_code == 200:
+                                        response_md5 = bytes_md5sum(r.content)
+                                        if response_md5 == block_md5:
+                                            blocks_md5.append(response_md5)
+                                            fp.write(r.content)
+                                            block_success = True
+                                            break
+                                        else:
+                                            LOG.error("checksum not equal, need %s, get %s", block_md5, response_md5)
+                                            block_success = False
+                                            continue
+                                    else:
+                                        LOG.error("error:\ncode: %s\ncontent: %s", r.status_code, r.content)
+                                        block_success = False
+                                        continue
+                                if not block_success:
                                     success = False
                                     break
                             else:
                                 LOG.error("not enough data nodes online")
                                 success = False
                                 break
-                        fp.close()
                         if success:
-                            result = True
+                            fp.close()
+                            checksum = strings_md5sum(blocks_md5)
+                            if file_info["checksum"] == checksum:
+                                result = True
+                            else:
+                                LOG.error("download file[%s => %s] failed, checksum not equal: %s", remote_path, local_path, checksum)
                         else:
                             LOG.error("download file[%s => %s] failed", remote_path, local_path)
                     else:
